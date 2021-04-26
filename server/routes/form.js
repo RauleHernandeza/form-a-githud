b4c0d0376cd53e842479706f567f1392a56d716a
Roberto Acosta <robetorobatox186@gmail.com>
2021-04-25 23:11:56 -0400

    li

--- server/routes/form.js
+++ server/routes/form.js
@@ -9,6 +9,7 @@ app.post('/create', function (req, res) {
   
      form.createForm(info).then(resp=>{
          let rest=resp.rows[0];
+         console.log(rest)
             let j=-1;
             console.log(info.preguntas.length)
          for(i=0;i<info.preguntas.length;i++){
