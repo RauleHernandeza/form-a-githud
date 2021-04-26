3ba57033ccf181953c54e997a01ad2e8f7f91a68
Roberto Acosta <robetorobatox186@gmail.com>
2021-04-26 06:42:31 -0400

    h

--- server/helpers/preguntas.js
+++ server/helpers/preguntas.js
@@ -15,7 +15,7 @@ return h;
 }
 module.exports.selectpregunta= async ()=>{
     const client = new Client({connectionString:"postgres://esvlqmxr:ShL33mmvfoTZHNr7qRtY55xlkWKxvC8f@queenie.db.elephantsql.com:5432/esvlqmxr"})
-    console.log(info)
+    
     client.connect()
     
 var h=await  client.query(prop.selectPregunta,[]);
